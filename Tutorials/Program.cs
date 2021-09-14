fb40b23b3d4ac1269a3452145acf12427dbc708b
MariyaBukh <85184644+MariyaBukh@users.noreply.github.com>
2021-09-14 10:56:08 -0400

    tutorials

---
+++ Tutorials/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+
+            System.Console.WriteLine($"You typed: {input}");
+
+        
+        }
+    }
+}
