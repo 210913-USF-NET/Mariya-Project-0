d6a71989be5411f120ea5d222ebc1cd90d79dbaa
MariyaBukh <85184644+MariyaBukh@users.noreply.github.com>
2021-09-15 11:40:26 -0400

    build archetecture

---
+++ Project 0/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Models
+{
+    public class Customer
+    {
+        public Customer(){}
+
+        public Customer(string name){
+            this.Name = name;
+        }
+
+        public Customer( string name, int age) : this (name)
+        {
+            this.Age = age;
+
+        }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
